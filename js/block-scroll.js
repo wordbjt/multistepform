62588bf0a4dd66b7f541eab9d554800c268a218b
wordbjt <wordbjt@users.noreply.github.com>
2022-01-27 20:17:08 +0800

    Update block-scroll.js

--- js/block-scroll.js
+++ js/block-scroll.js
@@ -81,9 +81,6 @@
 
       $(".next").click(function() {
         goDown();
-	var validator = $( "#myform" ).validate();
-	var currentInput = $(".active-div input").not(":input[type=button]");
-	validator.element(currentInput);
       });
 
 
