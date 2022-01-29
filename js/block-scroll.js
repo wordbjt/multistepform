57878dd4c3384f43a411e17298eec4fdc715bbc5
wordbjt <wordbjt@users.noreply.github.com>
2022-01-29 13:23:49 +0800

    Update block-scroll.js

--- js/block-scroll.js
+++ js/block-scroll.js
@@ -139,18 +139,18 @@
         gotoDiv(activeDiv+1);
 	$(theDivs[activeDiv]).addClass("active-div");
 	$(theDivs[activeDiv-1]).removeClass("active-div");
-      }
-	    
+	
 	// If has required-field disable next button
 						       
         //if ($('.required-field.active-div input, .required-field.active-div textarea').not(':button').valid()) {
 	  if ($(theDivs[activeDiv]).hasClass(".required-field")) {
-	      $('.required-field.active-div .next').prop('disabled', 'disabled');
+	      $('.active-div .next').prop('disabled', 'disabled');
 	    //$('.required-field.active-div .next').css("display", "inline-block");
         } else {
-	      $('.required-field.active-div .next').prop('disabled', false); 
+	      $('.active-div .next').prop('disabled', false); 
 	    //$('.required-field.active-div .next').css("display", "none");
-        }	    
+        }
+      }	    
     }
 
     function gotoDiv(number)
