d85c0732570c7a89316e7366c4dae1067f01f25a
wordbjt <wordbjt@users.noreply.github.com>
2022-02-05 21:30:26 +0800

    Update block-scroll.js

--- js/block-scroll.js
+++ js/block-scroll.js
@@ -114,7 +114,7 @@
 		//	}
 		//});
 	    
-$(theDivs[activeDiv] fieldset).on('scroll', function() {
+$('theDivs[activeDiv]' fieldset).on('scroll', function() {
 //$('.active-div fieldset').on('scroll', function() {
   var scrollTop = $(this).scrollTop();
   if (scrollTop + $(this).innerHeight() >= this.scrollHeight) {
