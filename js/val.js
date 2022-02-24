feec73a775715b93d5f2a088242b7b119ef99c16
wordbjt <wordbjt@users.noreply.github.com>
2022-02-24 22:05:32 +0800

    Create val.js

---
+++ js/val.js
@@ -0,0 +1,258 @@
+// VALIDATE
+
+$(document).ready(function() {
+	$("#msform").validate({
+
+// SUBMIT
+
+submitHandler: function(form) {
+var prod ="https://webto.salesforce.com/servlet/servlet.WebToLead?encoding=UTF-8";
+console.log('valid form submission');
+
+    $.ajax({
+        url: prod,
+        type: 'post',
+        dataType: 'jsonp',
+        jsonp: '$callback',
+        data: $('#msform *').serialize()
+        //success: function(data) {
+        //    $('#msform').hide();
+        //    $("#success-message").show();
+	//    console.log('Show success');
+        //}
+    });
+
+	console.log("Submitted");
+
+        $('#msform').hide();
+        $('#success-message').fadeIn(750);
+
+    return false;
+},
+
+// other options
+
+	  ignore: [],
+
+rules: {
+          name: {
+              required: true,
+              minlength: 3
+          },
+          address: {
+              required: true,
+              minlength: 3
+          },
+          suburb: {
+              required: true,
+              minlength: 3
+          },
+          state: {
+              required: true,
+              minlength: 2
+          },
+          postcode: {
+              required: true,
+              minlength: 4
+          },
+          phone: {
+              required: true,
+              minlength: 4
+          },
+          email: {
+              required: true,
+              email: true
+          },
+    	  contactpreference: {
+      		required: true
+    	  },
+	  fundraise: {
+      		required: true
+    	  },
+	  fundraise_pack: {
+      		//required: true
+    	  },
+	  event_title: {
+      		//required: true
+    	  },
+	  venue: {
+      		//required: true
+    	  },
+	  start_date: {
+      		//required: true
+    	  },
+	  start_day: {
+      		//required: true,
+		digits: true,
+		maxlength: 2
+    	  },
+	  start_month: {
+      		//required: true,
+		digits: true,
+		maxlength: 2
+    	  },
+	  start_year: {
+      		//required: true,
+		digits: true
+    	  },
+	  finish_date: {
+      		//required: true
+    	  },
+	  finish_day: {
+      		//required: true,
+		digits: true,
+		maxlength: 2
+    	  },
+	  finish_month: {
+      		//required: true,
+		digits: true,
+		maxlength: 2
+    	  },
+	  finish_year: {
+      		//required: true,
+		digits: true
+    	  },
+          time: {
+      		//required: true
+    	  },
+	  participants: {
+      		//required: true,
+		digits: true
+    	  },
+	  fundraising_activity: {
+      		//required: true
+    	  },
+	  fundraise_destination: {
+      		//required: true
+    	  },
+	  insurance: {
+      		//required: true
+    	  },
+	  guidelines: {
+      		//required: true
+    	  },
+	  declaration: {
+      		//required: true
+    	  }
+
+      },
+
+    // change name of error class that is assigned to input fields and placement
+
+errorPlacement:
+function( error, element ){
+if(element.is( ":radio" )){
+// error append here
+//error.appendTo('.active-div div.action-wrapper');
+error.prepend('.active-div div.action-wrapper');
+}
+else {
+error.insertAfter(element);
+}
+}
+
+    //errorClass: 'error_validate',
+    //errorPlacement: function (label, element) {
+
+    //     default
+    //    if (element.is(':radio')) {
+           //label.insertAfter(element.next('label'));
+	   //label.insertBefore(element('div.action-wrapper'));
+
+      // }
+      //  else {
+      //      label.insertAfter(element);
+      //  }
+    //}
+
+});
+
+  
+   $('#conditional input[type="radio"]').click(function() {
+    if($(this).attr('id') == 'checkbox-online') {
+    $('#submit-form-11').fadeIn('slow');
+    $('#continue-form-11').fadeOut('slow');
+    $('.conditional-section').hide();
+
+    $("input[name='fundraise_pack']").attr("required", false);
+    $("input[name='event_title']").attr("required", false);
+    $("input[name='venue']").attr("required", false);
+    $("input[name='start_date']").attr("required", false);
+    $("input[name='start_day']").attr("required", false);
+    $("input[name='start_month']").attr("required", false);
+    $("input[name='start_year']").attr("required", false);
+    $("input[name='finish_date']").attr("required", false);
+    $("input[name='finish_day']").attr("required", false);
+    $("input[name='finish_month']").attr("required", false);
+    $("input[name='finish_year']").attr("required", false);
+    $("input[name='time']").attr("required", false);
+    $("input[name='participants']").attr("required", false);
+    $("input[name='fundraising_activity']").attr("required", false);
+    $("input[name='fundraising_destination']").attr("required", false);
+    $("input[name='insurance']").attr("required", false);
+    $("input[name='guidelines']").attr("required", false);
+    $("input[name='declaration']").attr("required", false);
+
+  } else {
+    $("input[name='fundraise_pack']").attr("required", true);
+    $("input[name='event_title']").attr("required", true);
+    $("input[name='venue']").attr("required", true);
+    $("input[name='start_date']").attr("required", true);
+    $("input[name='start_day']").attr("required", true);
+    $("input[name='start_month']").attr("required", true);
+    $("input[name='start_year']").attr("required", true);
+    $("input[name='finish_date']").attr("required", true);
+    $("input[name='finish_day']").attr("required", true);
+    $("input[name='finish_month']").attr("required", true);
+    $("input[name='finish_year']").attr("required", true);
+    $("input[name='time']").attr("required", true);
+    $("input[name='participants']").attr("required", true);
+    $("input[name='fundraising_activity']").attr("required", true);
+    $("input[name='fundraising_destination']").attr("required", true);
+    $("input[name='insurance']").attr("required", true);
+    $("input[name='guidelines']").attr("required", true);
+    $("input[name='declaration']").attr("required", true);
+
+    $('#submit-form-11').fadeOut('slow');
+    $('#continue-form-11').fadeIn('slow');
+    $('.conditional-section:not("#declaration")').show();
+  }
+});  
+  
+// TYPING VALIDATION
+//setup before functions
+var typingTimer;                // timer identifier
+var doneTypingInterval = 500;  // time in ms, 0.5 second for example
+var $input = $('input, textarea');
+
+//on keyup, start the countdown
+$input.on('keyup', function () {
+  clearTimeout(typingTimer);
+  typingTimer = setTimeout(doneTyping, doneTypingInterval);
+});
+
+//on keydown, clear the countdown
+$input.on('keydown', function () {
+  clearTimeout(typingTimer);
+});
+
+//user is "finished typing," do something
+function doneTyping () {
+  //do something
+  if ($(".active-div input, .active-div textarea").not(':button').valid()) {
+    $('.active-div .next').prop('disabled', false);
+} else {
+    $('.active-div .next').prop('disabled', 'disabled');
+}
+}
+  
+// Enable Disable Next Button
+    $('input[type=time]').on('blur', function() {
+        if ($(".active-div input[type=time]").valid()) {
+            $('.active-div .next').prop('disabled', false);
+        } else {
+            $('.active-div .next').prop('disabled', 'disabled');
+        }
+    });  
+
+});
